724014746356547632d10ef780cd66a9c4d3a41c
Ken Buu <nongtrungbuu1903@gmail.com>
2021-09-12 17:00:56 +0700

    commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Waiting',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Progress',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Order',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Food',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
---
+++ src/components/Waiting/Waiting.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from '../Button'
+
+class Waiting extends Component {
+
+    render() {
+        return (
+            <div>
+                <Button>Sign Up</Button>
+            </div>
+
+        )
+    }
+}
+
+export default Waiting;
